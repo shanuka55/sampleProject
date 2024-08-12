a67ce2b5966a3af598f2fa841d78f4c7d6830cd7
shanuka55 <shanukalakshan5555@gmail.com>
2024-08-12 16:25:02 +0530

    UserServices updatd

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,4 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class UserService {
+    @Autowired
+    private UserRepo userRepo;
 }
