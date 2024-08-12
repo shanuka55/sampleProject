110403cd523e06f6b48a09a09d3ae0299ad9d5f7
shanuka55 <shanukalakshan5555@gmail.com>
2024-08-12 16:00:32 +0530

    Created User Model

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,20 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
+    @Id
+    private int id;
+    private String name;
+
 }
