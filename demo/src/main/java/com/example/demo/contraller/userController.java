4ed5fbdb5f6338ba69e6dee3d52bb8e8d1562f97
shanuka55 <shanukalakshan5555@gmail.com>
2024-08-13 09:47:21 +0530

    finished update user maping

--- demo/src/main/java/com/example/demo/contraller/userController.java
+++ demo/src/main/java/com/example/demo/contraller/userController.java
@@ -1,17 +1,31 @@
 package com.example.demo.contraller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping(value = "api/v1/")
 public class userController {
-    @GetMapping("/getUser")
-    public String getUser() {
-        return "All users";
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getusers")
+    public List<UserDTO> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/addusers")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO) {
+        return userService.saveUser(userDTO);
+    }
+    @PutMapping("/updateuser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO) {
+        return userService.updateUser(userDTO);
     }
 }
 
--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -1,13 +1,37 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.User;
 import com.example.demo.repo.UserRepo;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class UserService {
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public <list> List<UserDTO> getAllUsers() {
+       List<User>userList = userRepo.findAll();
+       return modelMapper.map(userList,new TypeToken<List<UserDTO>>(){}.getType());
+    }
+
+    public UserDTO saveUser(UserDTO userDTO) {
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+
+    public UserDTO updateUser(UserDTO userDTO) {
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
 }
